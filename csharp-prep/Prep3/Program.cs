1291d29d602daa5046b4cece26c1ccf0bad9092e
T-vista <tylervistaunet@gmail.com>
2023-01-11 07:28:11 -0700

    proj 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int random = randomGenerator.Next(1, 100);
+        int number = 0;
+        do 
+        {
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            number = int.Parse(guess);
+            if (number > random)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (number < random)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number == random)
+            {
+                Console.WriteLine("That is correct ");
+            }
+        } while (number != random);
     }
 }
